0636eb8d706e43d62606761c15f70d74984b7f78
Phylicia Bediako <phylicia.bediako@gmail.com>
2025-06-12 20:59:58 -0600

    l03buttons

--- L03_EC_ModifytwoButton/src/L03_EC_ModifytwoButton.cpp
+++ L03_EC_ModifytwoButton/src/L03_EC_ModifytwoButton.cpp
@@ -1,10 +1,18 @@
 /* 
- * Project L03_03_twoButtonLED
+ * Project L03_EC_ModifytwoButton
  * Author: Phylicia Bediako
  * Date: June 12, 2025
  * For comprehensive documentation and examples, please visit:
  * https://docs.particle.io/firmware/best-practices/firmware-template/
  */
+
+ /* Description:
+2 LEDs, 2 buttons
+Green LED @ Pin D6, Yellow LED @ Pin D5
+Button 1 @ D3, Button 2 @ D2
+Green LED lights up if both buttons are pressed
+Yellow LED lights up if either button is pressed
+No print button states to the screen*/
 
 // Include Particle Device OS APIs
 #include "Particle.h"
@@ -12,18 +20,11 @@
 // Let Device OS manage the connection to the Particle Cloud
 SYSTEM_MODE(SEMI_AUTOMATIC);
 
-/*
-First button (GRAY) to D3, power Green LED in Pin D6
-Add a second button (RED) to D2, power Yellow LED in Pin D5.
-Have each button control one LED.
-Also, print button states to the screen.*/
-
 const int GRAYBUTTON=D3;
 const int REDBUTTON=D2;
 const int GRNLED=D6;
 const int YELOLED=D5;
 bool buttonState1, buttonState2;
-
 
 void setup() {
   pinMode(GRAYBUTTON, INPUT_PULLDOWN);
@@ -34,23 +35,25 @@ void setup() {
   waitFor(Serial.isConnected, 10000);
 }
 
-
+/*The Green LED lights up if both buttons are pressed
+The Yellow LED lights up if either button is pressed*/
 void loop() {
   buttonState1=digitalRead(GRAYBUTTON);
-  if(buttonState1 == 1){
+  buttonState2=digitalRead(REDBUTTON);
+
+  if((buttonState1) && (buttonState2)){
     digitalWrite(GRNLED,HIGH);
   }
   else{
     digitalWrite(GRNLED,LOW);
   }
 
-  buttonState2=digitalRead(REDBUTTON);
-  if(buttonState2 == 1){
+  if(buttonState1 || buttonState2){
     digitalWrite(YELOLED,HIGH);
   }
   else{
     digitalWrite(YELOLED,LOW);
   }
 
-  Serial.printf("ButtonState1 = %i, ButtonState2 = %i\n",buttonState1,buttonState2);
+  //Serial.printf("ButtonState1 = %i, ButtonState2 = %i\n",buttonState1,buttonState2);
 }
